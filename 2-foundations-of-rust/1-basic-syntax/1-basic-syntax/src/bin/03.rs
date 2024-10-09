ece0ac63a7337aa84b458f68c95e959b2409bd45
Paraschiva George-Theodor <paraschiva.george.theodor@gmail.com>
2024-10-09 16:36:29 +0000

    asasd

--- 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
+++ 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
@@ -2,17 +2,21 @@ fn main() {
     let input:[i32; 8] = [23, 82, 16, 45, 21, 94, 12, 34];
 
     // TODO
-
+/* 
     let mut max = 0;let mut min = 100;
 
-    for i in 0..8 {
-        if input[i] < min {
-            min = input[i];
+    for i in input {
+        if i < min {
+            min = i;
         } 
-        if input[i] > max {
-            max = input[i];
+        if i > max {
+            max = i;
         }
     }
+*/
 
-    println!("{} is largest and {} is smallest", max, min);
+    let max = input.iter().max().unwrap_or(&0);
+    let min = input.iter().min().unwrap();
+
+    println!("{:?} is largest and {} is smallest", max, min);
 }
